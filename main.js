a5775eaab77e03bcfe37da1b9902f6bed6d7423c
unknown <naman.gupta@everest.engineering>
2021-06-03 15:54:00 +0530

    Added multiplication Feature

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function divideByTwo(additionValue){
-    let f = additionValue/2;
-    console.log(f);
+    let dividedValue = additionValue/2;
+    console.log(dividedValue);
+    mutliplication(dividedValue);
 };
-
 divideByTwo(5)
+
+
+function mutliplication(dividedValue){
+    let multiple=dividedValue*2
+    console.log(multiple);
+}
